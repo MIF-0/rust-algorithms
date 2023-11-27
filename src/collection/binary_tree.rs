8a8153625cd33305be7402ba878bb76d993f8a93
Vladyslav Bukatin <vladyslav.bukatin@complyadvantage.com>
2023-11-27 20:26:41 +0000

    Delete operation for the binary tree

--- src/collection/binary_tree.rs
+++ src/collection/binary_tree.rs
@@ -1,5 +1,6 @@
 use std::cmp::Ordering;
 use std::marker::PhantomData;
+use std::mem;
 use std::ptr::NonNull;
 
 pub struct BinarySearchTree<K, V>
@@ -20,6 +21,15 @@ where
     value: V,
     left: Link<K, V>,
     right: Link<K, V>,
+}
+
+struct DeleteResult<K, V>
+where
+    K: Ord,
+{
+    new_link: Link<K, V>,
+    deleted_key: K,
+    deleted_value: V,
 }
 
 impl<K, V> Node<K, V>
@@ -85,7 +95,7 @@ where
     }
 
     pub fn put(&mut self, key: K, value: V) {
-        self.root = BinarySearchTree::insert(self.root, key, value);
+        self.root = BinarySearchTree::upsert(self.root, key, value);
         self.length += 1;
     }
 
@@ -97,21 +107,6 @@ where
                 Some(&current.value)
             },
         };
-    }
-
-    unsafe fn find_min<'a>(root_link: NonNull<Node<K, V>>) -> &'a Node<K, V> {
-        let mut current = root_link.as_ref();
-        while current.left.is_some() {
-            match current.left {
-                None => {
-                    break;
-                }
-                Some(link) => {
-                    current = link.as_ref();
-                }
-            }
-        }
-        current
     }
 
     pub fn max(&self) -> Option<&V> {
@@ -137,26 +132,141 @@ where
     }
 
     pub fn delete_min(&mut self) {
-        if self.root.is_none() {
-            return;
-        }
-
-        unsafe {
-            self.root = Self::_delete_min(self.root);
+        match self.root {
+            None => {
+                //do nothing
+            }
+            Some(root) => unsafe {
+                let result = Self::_delete_min(root);
+                self.root = result.new_link;
+            },
         }
     }
 
-    unsafe fn _delete_min(link: Link<K, V>) -> Link<K, V> {
-        match link {
-            None => None,
+    pub fn delete(&mut self, key: K) {
+        match self.root {
+            None => {
+                //do nothing
+            }
             Some(root) => {
-                let root_ref = root.as_ptr();
-                if (*root_ref).left.is_none() {
-                    let v = Box::from_raw(root_ref);
-                    return v.right;
+                unsafe {
+                    let result = Self::_delete(root, key);
+                    match result {
+                        None => {
+                            //do nothing
+                        }
+                        Some(value) => {
+                            self.root = value.new_link;
+                        }
+                    }
                 }
-                (*root_ref).left = Self::_delete_min((*root_ref).left);
-                link
+            }
+        }
+    }
+    unsafe fn find_min<'a>(root_link: NonNull<Node<K, V>>) -> &'a Node<K, V> {
+        let mut current = root_link.as_ref();
+        while current.left.is_some() {
+            match current.left {
+                None => {
+                    break;
+                }
+                Some(link) => {
+                    current = link.as_ref();
+                }
+            }
+        }
+        current
+    }
+
+    unsafe fn _delete(root: NonNull<Node<K, V>>, key: K) -> Option<DeleteResult<K, V>> {
+        let root_ptr = root.as_ptr();
+        let compare_result = key.cmp(&(*root_ptr).key);
+        match compare_result {
+            Ordering::Less => {
+                let left = (*root_ptr).left?;
+                let result = Self::_delete(left, key)?;
+                (*root_ptr).left = result.new_link;
+                Some(DeleteResult {
+                    new_link: Some(root),
+                    deleted_key: result.deleted_key,
+                    deleted_value: result.deleted_value,
+                })
+            }
+            Ordering::Greater => {
+                let right = (*root_ptr).right?;
+                let result = Self::_delete(right, key)?;
+                (*root_ptr).right = result.new_link;
+                Some(DeleteResult {
+                    new_link: Some(root),
+                    deleted_key: result.deleted_key,
+                    deleted_value: result.deleted_value,
+                })
+            }
+            Ordering::Equal => {
+                match ((*root_ptr).left, (*root_ptr).right) {
+                    (Some(_), Some(right)) => {
+                        // find a min in the right sub tree
+                        let deleted_min_result = Self::_delete_min(right);
+                        let deleted_key =
+                            mem::replace(&mut (*root_ptr).key, deleted_min_result.deleted_key);
+                        let deleted_value =
+                            mem::replace(&mut (*root_ptr).value, deleted_min_result.deleted_value);
+                        let final_result = DeleteResult {
+                            new_link: Some(root),
+                            deleted_key,
+                            deleted_value,
+                        };
+                        Some(final_result)
+                    }
+                    (None, Some(_)) => {
+                        let v = Box::from_raw(root_ptr);
+                        Some(DeleteResult {
+                            new_link: v.right,
+                            deleted_key: v.key,
+                            deleted_value: v.value,
+                        })
+                    }
+                    (Some(_), None) => {
+                        let v = Box::from_raw(root_ptr);
+                        Some(DeleteResult {
+                            new_link: v.left,
+                            deleted_key: v.key,
+                            deleted_value: v.value,
+                        })
+                    }
+                    (None, None) => {
+                        let v = Box::from_raw(root_ptr);
+                        Some(DeleteResult {
+                            new_link: None,
+                            deleted_key: v.key,
+                            deleted_value: v.value,
+                        })
+                    }
+                }
+            }
+        }
+    }
+
+    unsafe fn _delete_min(root: NonNull<Node<K, V>>) -> DeleteResult<K, V> {
+        let root_ref = root.as_ptr();
+
+        match (*root_ref).left {
+            None => {
+                let v = Box::from_raw(root_ref);
+                DeleteResult {
+                    new_link: v.right,
+                    deleted_key: v.key,
+                    deleted_value: v.value,
+                }
+            }
+            Some(left) => {
+                let result = Self::_delete_min(left);
+                (*root_ref).left = result.new_link;
+                DeleteResult {
+                    new_link: Some(root),
+                    deleted_key: result.deleted_key,
+                    deleted_value: result.deleted_value,
+                }
             }
         }
     }
@@ -182,7 +292,7 @@ where
         BinarySearchTree::remove_tree(right_node);
     }
 
-    fn insert(node_link: Link<K, V>, key: K, value: V) -> Link<K, V> {
+    fn upsert(node_link: Link<K, V>, key: K, value: V) -> Link<K, V> {
         match node_link {
             None => {
                 let new_node = Node::new(key, value);
@@ -200,14 +310,14 @@ where
                     match compare_result {
                         Ordering::Less => {
                             (*ptr_node).left =
-                                BinarySearchTree::insert((*ptr_node).left, key, value);
+                                BinarySearchTree::upsert((*ptr_node).left, key, value);
                         }
                         Ordering::Equal => {
                             (*ptr_node).value = value;
                         }
                         Ordering::Greater => {
                             (*ptr_node).right =
-                                BinarySearchTree::insert((*ptr_node).right, key, value);
+                                BinarySearchTree::upsert((*ptr_node).right, key, value);
                         }
                     };
                 }
@@ -230,6 +340,7 @@ where
 mod test {
     use crate::collection::binary_tree::BinarySearchTree;
     use easy_assert::num_assertions::NumericAssert;
+    use easy_assert::option_assertions::OptionAssert;
     use easy_assert::string_assertions::StringAssert;
     use easy_assert::{actual, expected};
 
@@ -362,5 +473,29 @@ mod test {
         StringAssert::assert_that(actual(result.to_string()))
             .is_equal()
             .to(expected("D".to_string()));
+    }
+
+    #[test]
+    fn basics_delete() {
+        // GIVEN
+        let mut tree: BinarySearchTree<usize, &str> = BinarySearchTree::new();
+        tree.put(10, "T");
+        tree.put(4, "D");
+        tree.put(1, "A");
+        tree.put(7, "K");
+
+        let result = tree.get(7).copied().unwrap_or("IT IS EMPTY");
+
+        StringAssert::assert_that(actual(result.to_string()))
+            .is_equal()
+            .to(expected("K".to_string()));
+
+        // WHEN
+        tree.delete(7);
+
+        // THEN
+        let result = tree.get(7).copied();
+
+        OptionAssert::assert_that(result).is_none();
     }
 }
